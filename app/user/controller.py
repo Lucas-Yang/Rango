1fd4e804fc8e29405875d3cbbc7037e31ffb712a
luoyadong <luoyadong@bilibili.com>
2021-07-23 11:37:52 +0800

    first jwt

--- app/user/controller.py
+++ app/user/controller.py
@@ -14,7 +14,7 @@ user_app = APIRouter()
 format_handler = FormatCheck()
 
 
-@user_app.post('/register', response_model=UserModelReturn)
+@user_app.post('/register', response_model=UserModelReturn, summary="用户账号注册")
 def user_register(item: UserRegisterItem):
     """
     :return:
@@ -30,7 +30,7 @@ def user_register(item: UserRegisterItem):
         return UserModelReturn(code=1, msg="input error")
 
 
-@user_app.put('/update', response_model=UserModelReturn)
+@user_app.put('/update', response_model=UserModelReturn, summary="用户信息更新，主要是root管理员调用")
 async def user_update(item: UserUpdateItem):
     """
     :return:
@@ -46,7 +46,7 @@ async def user_update(item: UserUpdateItem):
         return UserModelReturn(code=1, msg="input error")
 
 
-@user_app.get('/status')
+@user_app.get('/status', response_model=UserModelReturn, summary="用户身份查询")
 async def user_status(user_id: str):
     """
     :return:
@@ -62,26 +62,18 @@ async def user_status(user_id: str):
         return UserModelReturn(code=1, msg="input error")
 
 
-@user_app.post('/login', response_model=UserModelReturn)
+@user_app.post('/login', response_model=UserModelReturn, summary="用户登录（会自动调用获取token的api)")
 async def user_login(item: UserLoginItem):
     """
     :return:
     """
     if format_handler.user_register_check(item):
         user_handler = UserModel(item.dict())
-        reg_status, msg = user_handler.user_register()
+        reg_status, msg = user_handler.user_login()
         if reg_status:
             return UserModelReturn(code=0, msg="success", data={"info": msg})
         else:
             return UserModelReturn(code=2, msg="internal error", data={"info": msg})
     else:
         return UserModelReturn(code=1, msg="input error")
-
-
-
-
-
-
-
-
 
