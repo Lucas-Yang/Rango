315759fbb34951288f111e130e2b49fd7d8b5114
luoyadong <luoyadong@bilibili.com>
2021-08-11 17:57:56 +0800

    celery task

--- begin.sh
+++ begin.sh
@@ -8,7 +8,7 @@ then
   cd /data/app/rango
   cp -r /data/app/rango/config/prodconfig.py /data/app/rango/config/config.py
   # /home/work/.local/bin/celery -A app.bin.tasks worker -l info
-  python /data/app/rango/main.py --reload=False
+  python /data/app/rango/main.py --reload=False & /home/work/.local/bin/celery -A app.bin.tasks worker -l info
 else
   cp -r config/testconfig.py config/config.py
   python main.py & celery -A app.bin.tasks  worker -l info
