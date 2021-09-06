61aa7d85894c22fac7589ed497f6aad1b99515c9
沈煜超 <shenyuchao@bilibili.com>
2021-09-06 17:01:06 +0800

    add record_tagging_task_user expire_data

--- app/bin/dao.py
+++ app/bin/dao.py
@@ -2,7 +2,7 @@ import app.bin.model as model
 from app.common.db import MyMongoClient
 from app.common import mongodb_client
 import time
-
+from datetime import datetime
 # from app.bin.utils.Evaluation import FRVideoEvaluationFactory
 # from app.bin.utils.Evaluation import NRVideoEvaluationFactory
 """ 为接口生产数据
@@ -23,7 +23,7 @@ class TaggingDao(object):
         res = task_col.find_one({'task_id': task_id}, {'_id': 0})
         return res
 
-    def query_tagging_task_by_index(self, task_id,index):
+    def query_tagging_task_by_index(self, task_id, index):
         task_col = self.db['rango_evaluate_tasks']
         res = task_col.find_one({'task_id': task_id}, {'groups': 1})
         res = res.get('groups')
@@ -88,11 +88,13 @@ class TaggingDao(object):
 
     def record_tagging_task_user(self, tasks_info: dict):
         task_col = self.db['rango_evaluate_tasks']
-        task_name = task_col.find_one({'task_id': tasks_info['task_id']}, {'task_name': 1})
-
+        task_info = task_col.find_one({'task_id': tasks_info['task_id']},
+                                      {'task_name': 1, 'expire_data': 1})
+        print('task_info',task_info)
         score_col = self.db['rango_tagging_users']
         select = {'task_id': tasks_info['task_id'], 'user': tasks_info['user']}
-        update_set = {"$set": {'task_id': tasks_info['task_id'],'task_name':task_name,
+        update_set = {"$set": {'task_id': tasks_info['task_id'], 'task_name': task_info['task_name'],
+                               'expire_data': task_info['expire_data'].strftime("%Y-%m-%d %H:%M:%S"),
                                'user': tasks_info['user'], 'status': tasks_info['status'],
                                'created_at': time.strftime("%Y-%m-%d %H:%M:%S")}}
         res = score_col.update_one(select, update_set, upsert=True)
